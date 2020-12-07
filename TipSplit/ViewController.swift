e59e11176860f6df0acbbc4f1493e67636582d93
PageKallop <PageMarie78@gmail.com>
2020-12-06 19:01:01 -0600

    First commit Built UI

--- TipSplit/ViewController.swift
+++ TipSplit/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UILabel!
+    
+    @IBOutlet weak var tenPctButton: UIButton!
+    
+    @IBOutlet weak var fifteenPctButton: UIButton!
+    
+    @IBOutlet weak var twentyPctButton: UIButton!
+    
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tenPctButton.layer.cornerRadius = 5
+        tenPctButton.layer.borderWidth = 2.0
+        tenPctButton.layer.borderColor = UIColor.black.cgColor
+        tenPctButton.layer.backgroundColor = UIColor.white.cgColor
+        
+        fifteenPctButton.layer.cornerRadius = 5
+        fifteenPctButton.layer.borderWidth = 2.0
+        fifteenPctButton.layer.borderColor = UIColor.black.cgColor
+        fifteenPctButton.layer.backgroundColor = UIColor.white.cgColor
+        
+        twentyPctButton.layer.cornerRadius = 5 
+        twentyPctButton.layer.borderWidth = 2.0
+        twentyPctButton.layer.borderColor = UIColor.black.cgColor
+        twentyPctButton.layer.backgroundColor = UIColor.white.cgColor
     }
-
-
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
 }
 
